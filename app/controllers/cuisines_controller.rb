52b872efa2e3e9f1987f28ef06fea0e4cff21d58
Joao e Rogerio <jhm.vale@gmail.com>
2017-07-04 18:54:49 -0300

    refector cuisine_controller

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -1,16 +1,13 @@
 class CuisinesController < ApplicationController
+  before_action :set_cuisine, only: [:show, :edit, :update]
   def show
-    @cuisine = Cuisine.find(params[:id])
   end
 
   def edit
-    @cuisine = Cuisine.find(params[:id])
   end
 
   def update
-    @cuisine = Cuisine.find(params[:id])
-
-    if @cuisine.update(name: params[:cuisine][:name])
+    if @cuisine.update(cuisine_params)
       redirect_to @cuisine
     else
       render 'edit'
@@ -22,11 +19,20 @@ class CuisinesController < ApplicationController
   end
 
   def create
-    @cuisine = Cuisine.new(params.require(:cuisine).permit(:name))
+    @cuisine = Cuisine.new(cuisine_params)
     if @cuisine.save
       redirect_to @cuisine
     else
       render 'new'
     end
+  end
+
+  private
+  def set_cuisine
+    @cuisine = Cuisine.find(params[:id])
+  end
+
+  def cuisine_params
+    params.require(:cuisine).permit(:name)
   end
 end
